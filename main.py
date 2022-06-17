4ab5975e9b0bccb5617de5e60e8fa90ba51b1610
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-17 15:36:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+print("Question 5: ?")
+print(" A. ","\n","B. ", "\n", "C. ", "\n", "D. ")
+print("\n")
+answer5 = ""
+
+while answer5 != "A" and answer5 != "B" and answer5 != "C" and answer5 != "D":
+  answer5 = input("What is the correct answer? ")
+  if answer5 == "A":
+    print(" Incorrect.")
+  elif answer5 == "B":
+    print(" Correct!.")
+  elif answer5 == "C":
+    print(" Incorrect.")
+  elif answer5 == "D":
+    print(" Incorrect.")
+  else:
+    print(" Invalid answer.  Try again.")
+
+print("\n")
+
+print("raisins")
+
